3bab92ac02f0b75f77c67b4a42035ce8c0c2b8be
girl-on-a-string <frenchfries55@protonmail.com>
2025-01-13 10:03:54 -0600

    added modal error div

--- src/index.js
+++ src/index.js
@@ -1,18 +1,29 @@
 import "./styles.css";
 import { modalControls, getModalData } from "./modal";
 
+let taskNameInput = document.getElementById("task-name");
+let taskDescInput = document.getElementById("task-desc");
+let priorityRadios = document.getElementsByName("priority");
+let modalErrors = document.getElementById("modal-error-display");
+
 document.getElementById("add-task-btn").addEventListener("click", () => {
     modalControls().openModal();
 });
 
+document.getElementById("cancel-action").addEventListener("click", () => {
+    taskNameInput.value = "";
+    taskDescInput.value = "";
+
+    modalControls().closeModal();
+});
+
 document.getElementById("submit-data").addEventListener("click", () => {
-    // getModalData();
 
-    let taskNameInput = document.getElementById("task-name").value;
-
-    if (taskNameInput !== "") {
+    if (taskNameInput.value !== "") {
         getModalData();
         modalControls().closeModal();
+    } else {
+        modalErrors.innerText = "Task must have a name and priority";
     }
 
     console.log(getModalData());
