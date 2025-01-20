28da271ede689d7751978c746703fb954710efdf
girl-on-a-string <frenchfries55@protonmail.com>
2025-01-20 10:21:52 -0600

    created checkinputs, checkradios, and checkname functions

--- src/modal.js
+++ src/modal.js
@@ -1,3 +1,7 @@
+// import
+
+import { newTask } from "./task";
+
 // open + close modal
 
 const form = document.getElementById("add-task-form");
@@ -25,18 +29,35 @@ const closeModal = () => {
 
 const submitModal = () => {
     // submit, close, and clear all data
-
     // throw error if no name and priority
-
     // create new task object with data
 
+    const nameInput = document.getElementById("task-name");
+    const descInput = document.getElementById("task-desc");
+    const priorityRadios = document.getElementsByName("priority");
+
+    const checkInputs = () => {
+        const checkRadios = () => {
+            for (const radio of priorityRadios) {
+                if (radio.checked) { 
+                    return true;
+                }
+            }
+        }
     
+        const checkName = () => {
+            if (nameInput.value !== "") {
+                return true;
+            }
+        }
+    }
+
 
 }
 
 const cancelModal = () => {
     // close and clear all data
-
+    
     form.reset();
     closeModal();
 }
