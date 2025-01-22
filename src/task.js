0dc92ddd7da393e1ac601049feec2c7dcb5aff15
girl-on-a-string <frenchfries55@protonmail.com>
2025-01-21 18:30:34 -0600

    tasks no longer dupicating with each add

--- src/task.js
+++ src/task.js
@@ -17,66 +17,64 @@ let taskList = [];
 // create task display
 
 const createDOMTask = () => {
-    taskList.forEach(() => {
-        let taskDisplay = document.getElementById("list");
+    let taskDisplay = document.getElementById("list");
     
-        // create main container
+    // create main container
     
-        let taskDiv = document.createElement("div");
-        taskDiv.classList.add("task-container");
-        taskDisplay.appendChild(taskDiv);
+    let taskDiv = document.createElement("div");
+    taskDiv.classList.add("task-container");
+    taskDisplay.appendChild(taskDiv);
     
-        // add title
+    // add title
     
-        let taskTitleDiv = document.createElement("div");
-        taskTitleDiv.innerText = "";
-        taskTitleDiv.classList.add("task-title");
-        taskDiv.appendChild(taskTitleDiv);
+    let taskTitleDiv = document.createElement("div");
+    taskTitleDiv.innerText = newTask().taskName;
+    taskTitleDiv.classList.add("task-title");
+    taskDiv.appendChild(taskTitleDiv);
     
-        // add desc 
+    // add desc 
     
-        let taskDescDiv = document.createElement("div");
-        taskDescDiv.innerText = "";
-        taskDescDiv.classList.add("task-desc");
-        taskDiv.appendChild(taskDescDiv);
+    let taskDescDiv = document.createElement("div");
+    taskDescDiv.innerText = newTask().taskDesc;
+    taskDescDiv.classList.add("task-desc");
+    taskDiv.appendChild(taskDescDiv);
     
-        // add priority
+    // add priority
     
-        let taskPriorityDiv = document.createElement("div");
-        taskPriorityDiv.innerText = "";
-        taskPriorityDiv.classList.add("task-priority");
-        taskDiv.appendChild(taskPriorityDiv);
+    let taskPriorityDiv = document.createElement("div");
+    taskPriorityDiv.innerText = newTask().taskPriority;
+    taskPriorityDiv.classList.add("task-priority");
+    taskDiv.appendChild(taskPriorityDiv);
     
-        // add edit btn
+    // add edit btn
     
-        let taskEditBtn = document.createElement("button");
-        taskEditBtn.innerText = "edit";
-        taskEditBtn.classList.add("task-edit-btn");
-        taskEditBtn.addEventListener("click", () => {
+    let taskEditBtn = document.createElement("button");
+    taskEditBtn.innerText = "edit";
+    taskEditBtn.classList.add("task-edit-btn");
+    taskEditBtn.addEventListener("click", () => {
     
-        });
-        taskDiv.appendChild(taskEditBtn);
-    
-        // add delete btn
-    
-        let taskDeleteBtn = document.createElement("button");
-        taskDeleteBtn.innerText = "delete";
-        taskDeleteBtn.classList.add("task-delete-btn");
-        taskDeleteBtn.addEventListener("click", () => {
-    
-        });
-        taskDiv.appendChild(taskDeleteBtn);
-    
-        // add complete btn
-    
-        let taskCompleteBtn = document.createElement("button");
-        taskCompleteBtn.innerText = "complete";
-        taskCompleteBtn.classList.add("task-complete-btn");
-        taskCompleteBtn.addEventListener("click", () => {
-    
-        });
-        taskDiv.appendChild(taskCompleteBtn);
     });
+    taskDiv.appendChild(taskEditBtn);
+    
+    // add delete btn
+    
+    let taskDeleteBtn = document.createElement("button");
+    taskDeleteBtn.innerText = "delete";
+    taskDeleteBtn.classList.add("task-delete-btn");
+    taskDeleteBtn.addEventListener("click", () => {
+    
+    });
+    taskDiv.appendChild(taskDeleteBtn);
+    
+    // add complete btn
+    
+    let taskCompleteBtn = document.createElement("button");
+    taskCompleteBtn.innerText = "complete";
+    taskCompleteBtn.classList.add("task-complete-btn");
+    taskCompleteBtn.addEventListener("click", () => {
+    
+    });
+    taskDiv.appendChild(taskCompleteBtn);
 }
 
 // edit task
