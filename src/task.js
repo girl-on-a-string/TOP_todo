b749783569cf5eeb65a18d4106cb8ecc19e1cfe4
girl-on-a-string <frenchfries55@protonmail.com>
2025-01-22 10:07:17 -0600

    redid dom elements according to schematic

--- src/task.js
+++ src/task.js
@@ -19,62 +19,104 @@ let taskList = [];
 const createDOMTask = (task) => {
     let taskDisplay = document.getElementById("list");
     
-    // create main container
+    // create main task container
     
     let taskDiv = document.createElement("div");
     taskDiv.classList.add("task-container");
     taskDisplay.appendChild(taskDiv);
-    
-    // add title
-    
-    let taskTitleDiv = document.createElement("div");
-    taskTitleDiv.innerText = task.taskName;
-    taskTitleDiv.classList.add("task-title");
-    taskDiv.appendChild(taskTitleDiv);
-    
-    // add desc 
-    
-    let taskDescDiv = document.createElement("div");
-    taskDescDiv.innerText = task.taskDesc;
-    taskDescDiv.classList.add("task-desc");
-    taskDiv.appendChild(taskDescDiv);
-    
-    // add priority
-    
-    let taskPriorityDiv = document.createElement("div");
-    taskPriorityDiv.innerText = task.taskPriority;
-    taskPriorityDiv.classList.add("task-priority");
-    taskDiv.appendChild(taskPriorityDiv);
-    
-    // add edit btn
-    
-    let taskEditBtn = document.createElement("button");
-    taskEditBtn.innerText = "edit";
-    taskEditBtn.classList.add("task-edit-btn");
-    taskEditBtn.addEventListener("click", () => {
-    
-    });
-    taskDiv.appendChild(taskEditBtn);
-    
-    // add delete btn
-    
-    let taskDeleteBtn = document.createElement("button");
-    taskDeleteBtn.innerText = "delete";
-    taskDeleteBtn.classList.add("task-delete-btn");
-    taskDeleteBtn.addEventListener("click", () => {
-    
-    });
-    taskDiv.appendChild(taskDeleteBtn);
-    
-    // add complete btn
-    
+
+    // create status container and content 
+
+    let statusDiv = document.createElement("div");
+    statusDiv.classList.add("task-status");
+    taskDiv.appendChild(statusDiv);
+
     let taskCompleteBtn = document.createElement("button");
     taskCompleteBtn.innerText = "complete";
     taskCompleteBtn.classList.add("task-complete-btn");
     taskCompleteBtn.addEventListener("click", () => {
     
     });
-    taskDiv.appendChild(taskCompleteBtn);
+    statusDiv.appendChild(taskCompleteBtn);
+
+    // create main content div 
+
+    let mainContentDiv = document.createElement("div");
+    mainContentDiv.classList.add("main-task-content");
+    taskDiv.appendChild(mainContentDiv);
+
+    // create task # div
+
+    let taskNumDiv = document.createElement("div");
+    taskNumDiv.classList.add("task-number");
+    taskNumDiv.innerText = "num";
+    mainContentDiv.appendChild(taskNumDiv);
+
+    // create task info div
+
+    let taskInfoDiv = document.createElement("div");
+    taskInfoDiv.classList.add("task-info");
+    mainContentDiv.appendChild(taskInfoDiv);
+
+        // add priority
+    
+        let taskPriorityDiv = document.createElement("div");
+        taskPriorityDiv.innerText = task.taskPriority;
+        taskPriorityDiv.classList.add("task-priority");
+        taskInfoDiv.appendChild(taskPriorityDiv);
+
+        // create main info div
+
+        let mainInfoDiv = document.createElement("div");
+        mainInfoDiv.classList.add("main-info");
+        taskInfoDiv.appendChild(mainInfoDiv);
+
+            // create title
+
+            let taskTitleDiv = document.createElement("div");
+            taskTitleDiv.innerText = task.taskName;
+            taskTitleDiv.classList.add("task-title");
+            mainInfoDiv.appendChild(taskTitleDiv);
+
+            // create desc
+
+            let taskDescDiv = document.createElement("div");
+            taskDescDiv.innerText = task.taskDesc;
+            taskDescDiv.classList.add("task-desc");
+            mainInfoDiv.appendChild(taskDescDiv);
+
+        // create due date div
+
+        let taskDueDiv = document.createElement("div");
+        taskDueDiv.classList.add("task-due-date");
+        taskDueDiv.innerText = "due date";
+        taskInfoDiv.appendChild(taskDueDiv);
+
+    // create task settings div
+
+    let taskSettingsDiv = document.createElement("div");
+    taskSettingsDiv.classList.add("task-settings");
+    taskDiv.appendChild(taskSettingsDiv);
+    
+        // create edit btn
+        
+        let taskEditBtn = document.createElement("button");
+        taskEditBtn.innerText = "edit";
+        taskEditBtn.classList.add("task-edit-btn");
+        taskEditBtn.addEventListener("click", () => {
+        
+        });
+        taskSettingsDiv.appendChild(taskEditBtn);
+        
+        // create delete btn
+        
+        let taskDeleteBtn = document.createElement("button");
+        taskDeleteBtn.innerText = "delete";
+        taskDeleteBtn.classList.add("task-delete-btn");
+        taskDeleteBtn.addEventListener("click", () => {
+        
+        });
+        taskSettingsDiv.appendChild(taskDeleteBtn);
 }
 
 // edit task
